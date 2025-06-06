21e329294bbc0e07559d8758f2479260c8166596
Tariku Temsgen <tarikooh120@gmail.com>
2025-06-06 16:59:04 +0300

    Database is working

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+$user = "root";
+$server = "localhost";
+$port = 3306;
+$database = "products";
+$pass = "Dontdosql101!";
+
+// $conn = mysqli_connect($server, $user, $pass, $database, $port);
+$conn = mysqli_connect("127.0.0.1", "root", "Dontdosql101!", "pl", "3306");
+if(!$conn){
+    die("Connection failed: " . mysqli_connect_error());
+}else echo "<h1>Successfully connected.<h1/>";
+
+?>
