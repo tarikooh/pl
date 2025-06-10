50594da85f73b5bd489aa9d1bf950f7e3df13a1a
Tariku Temsgen <tarikooh120@gmail.com>
2025-06-10 11:07:48 +0300

    logout deletes cookies now

--- public/logout.php
+++ public/logout.php
@@ -1,10 +1,12 @@
 <?php
 session_start();
 if(isset($_SESSION["username"])){
-    $_SESSION = array();
-    $_COOKIE = array();
+    $_SESSION = array();    
     session_destroy();  
+    $cookie_name = "User";
+    $cookie_value = $_COOKIE[$cookie_name];
+    setcookie($cookie_name, $cookie_value, time() - 3600, "/"); 
     header("Location: index.php");
-    
+
 }else header("Location: login.php");
 ?>
