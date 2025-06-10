758ef1ff99a84a09f75df5caa1a7671eeb6454c8
Tariku Temsgen <tarikooh120@gmail.com>
2025-06-10 11:02:41 +0300

    fix db.php

--- config/db.php
+++ config/db.php
@@ -1,12 +1,12 @@
 <?php
 $user = "root";
-$server = "localhost";
-$port = 3306;
-$database = "products";
+$server = "127.0.0.1";
+$port = "3306";
+$database = "pl";
 $pass = "Dontdosql101!";
 
-// $conn = mysqli_connect($server, $user, $pass, $database, $port);
-$conn = mysqli_connect("127.0.0.1", "root", "Dontdosql101!", "pl", "3306");
+$conn = mysqli_connect($server, $user, $pass, $database, $port);
+// $conn = mysqli_connect("127.0.0.1", "root", "Dontdosql101!", "pl", "3306");
 if(!$conn){
     die("Connection failed: " . mysqli_connect_error());
 }
